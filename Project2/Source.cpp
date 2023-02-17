ec4a9f01a90685371a5ebb0f0bd734a9b00cf57d
Sydney Jue <sydneyjue@gmail.com>
2023-02-17 10:22:06 -0800

    made text speed  faster and fixed confirm question

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -10,22 +10,50 @@ void typing(string hello)
 		cout << hello[x];
 		/*if (hello[x] != ' ' && hello[x] != '\n')
 			Beep(750, 300);*/
-		Sleep(100);
+		Sleep(50);
 		x++;
 	};
 }
 int main()
 {
+	char deci;
 	string intro = "Welcome\nIs this your first quest?\n";
 	string confirm;
+	
 	typing(intro);
 	cin >> confirm;
-	if (confirm == "no" || "No")
+	if (confirm == "no" || confirm == "No")
 	{
 		system("cls");
-		string begin = "\n";
+		string begin = "New Quest? Or Continue?\n";
+		typing(begin);
+		string decide;
+		cin >> decide;
+		if (decide == "New quest" || decide == "New Quest" ||decide == "new quest" || decide =="new")
+		{
+			deci = 'A';
+		}
+		if (decide == "Continue" || decide == "continue")
+		{
+			deci = 'B';
+		}
+		switch (deci)
+		{
+		case 'A': goto NEW;
+			break;
+		}
+		
+	}
+	if (confirm == "yes" ||confirm == "Yes")
+	{ 
+	NEW:
+		system("cls");
+		string begin1 = "How many members are in your party?\n";
+		typing(begin1);
+		int members;
+		cin >> members;
+		
 
 	}
-
 	return 0;
 }
